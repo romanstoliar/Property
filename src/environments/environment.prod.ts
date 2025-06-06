0d8e0532873e32ebc080f38085d45db1511af25c
Roman <knms1b22.stoliar@kpnu.edu.ua>
2025-06-06 22:03:52 +0300

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 	production: true,
 	appId: 'test',
 	//url: 'https://webart.work',
-	//url: '',
-	url: 'http://138.68.110.222:1546',
+	url: '',
+	//url: 'http://138.68.110.222:1546',
 
 	meta: {
 		title: 'Web Art Work',
